5145d9a9aeb298d2b8bd072abdc23b380b476356
Mayor-king <maylaw203@gmail.com>
2023-02-13 11:11:12 +0100

    Upated

---
+++ console.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+"""This the AirBnB console line interpreter"""
+
+import cmd
+import re
+import json
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class containing the entry point of the command interperter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Command to exit the program."""
+        exit()
+
+    def do_EOF(self, arg):
+        """Command to exit the program."""
+        return True
+
+    def help_quit(self):
+        print('Quit command to exit the program\n')
+
+    def help_create(self):
+        """`create` command help"""
+        print('Creates a new instance of BaseModel\n')
+
+    def help_show(self):
+        """`show` command help"""
+        print('Shows a BaseModel instance if exists\n')
+
+    def emptyline(self):
+        """Nothing happens on empty entered"""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if arg:
+            try:
+                kclass = globals().get(arg, None)
+                obj = kclass()
+                obj.save()
+                print(obj.id)
+            except Exception:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, arg):
+        """Shows an instance given its id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        strs = arg.split(" ")
+
+        if not strs:
+            print("** class name missing **")
+            return
+
+        kclass = globals().get(strs[0], None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+
+        if len(strs) != 2:
+            print("** instance id missing **")
+            return
+        obj_id = "{}.{}".format(strs[0], strs[1])
+        if obj_id in storage.all():
+            print(storage.all()[obj_id])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Destroys an instance based on he class name and id"""
+        if not arg:
+            print("** class name missing**")
+            return
+        strs = arg.split(" ")
+        count = len(strs)
+
+        if count == 0:
+            print("** class name missing**")
+            return
+        mName = strs[0]
+
+        kclass = globals().get(mName, None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+
+        if count < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = "{}.{}".format(strs[0], strs[1])
+
+        if obj_id not in storage.all():
+            print("** no instance found **")
+            return
+
+        obj = storage.all()[obj_id]
+        if obj.__class__.__name__ != mName:
+            print("** no instance found **")
+            return
+
+        storage.all().pop(obj_id, None)
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        kclass = globals().get(arg, None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+        for k, v in storage.all().items():
+            if v.__class__.__name__ != arg:
+                continue
+            print(v)
+
+    def do_count(self, arg):
+        """Print the count all class instances"""
+        kclass = globals().get(arg, None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+        count = 0
+        for obj in storage.all().values():
+            if obj.__class__.__name__ == arg:
+                count += 1
+        print(count)
+
+    def do_update(self, arg):
+        """Update an instance base of class name and id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        pattern = """^([A-Z][a-z]+)\s+([\w-]+)\s+([a-z_]+|\{[^}]+})(?:\s+("[^"]+"|\w+))?$"""
+
+        m = re.match(pattern, arg)
+        groups = [s for s in m.groups() if s] if m else []
+        count = len(groups)
+
+        if not groups:
+            print("** class name missing **")
+            return
+
+        kclass = globals().get(groups[0], None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+        mName = groups[0]
+
+        if count < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = "{}.{}".format(mName, groups[1])
+
+        if obj_id not in storage.all():
+            print("** no instance found **")
+            return
+
+        obj = storage.all()[obj_id]
+        if obj.__class__.__name__ != mName:
+            print("** no instance found **")
+            return
+
+        if count == 3:
+            """ dealing with a dictionary """
+            if groups[2][0] != '{':
+                print("** value missing **")
+                return
+
+            dictionary = json.loads(groups[2].replace("'", '"'))
+            for key, value in dictionary.items():
+                setattr(obj, key, value)
+            return
+        elif count < 3:
+            print("** attribute name missing **")
+            return
+
+        attrName = groups[2]
+
+        if count < 4:
+            print("** value missing **")
+            return
+
+        attrValue = None
+        try:
+            attrValue = int(groups[3])
+        except ValueError:
+            pass
+
+        if attrValue is None:
+            try:
+                attrValue = float(groups[3])
+            except ValueError:
+                pass
+
+        if attrValue is None:
+            attrValue = groups[3].replace('"', '')
+
+        setattr(obj, attrName, attrValue)
+        obj.save()
+
+    def default(self, arg):
+        if arg is None:
+            return
+
+        cmdPattern ="^([A-Za-z]+)\.([a-z]+)\(([^(]*)\)"
+        paramsPattern = """^"([^"]+)"(?:,\s*(?:"([^"]+)"|(\{[^}]+\}))(?:,\s*(?:("?[^"]+"?)))?)?"""
+        m = re.match(cmdPattern, arg)
+        if not m:
+            super().default(arg)
+            return
+        mName, method, params = m.groups()
+        m = re.match(paramsPattern, params)
+        params = [item for item in m.groups() if item] if m else []
+
+        cmd = " ".join([mName] + params)
+
+        if method == 'all':
+            return self.do_all(cmd)
+
+        if method == 'count':
+            return self.do_count(cmd)
+
+        if method == 'show':
+            return self.do_show(cmd)
+
+        if method == 'destroy':
+            return self.do_destroy(cmd)
+
+        if method == 'update':
+            return self.do_update(cmd)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+# test_base_model.py
+"""Defines unittests for base_model.py.
+
+Unittest classes:
+    TestBaseModel_Instantiation - line 15
+    TestBase_Instance_Print - line 51
+    TestBase_Save_Method - line 59
+    TestBase_from_json_string - line 234
+"""
+from datetime import datetime, timezone
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel_Instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+
+    def test_IsInstanceOf(self):
+        """Test instance"""
+        b1 = BaseModel()
+        self.assertIsInstance(b1, BaseModel)
+
+    def test_ContainsId(self):
+        """Test if id attribute exists"""
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "id"))
+
+    def test_IdType(self):
+        """Test if `id` attribute type"""
+        b1 = BaseModel()
+        self.assertEqual(type(b1.id), str)
+
+    def test_CompareTwoInstancesId(self):
+        """Compare distinct instances ids"""
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_ContainsCreated_at(self):
+        """Checks `created_at` attribute existence"""
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "created_at"))
+
+    def test_Created_atInstance(self):
+        """Checks `created_at` attribute's type"""
+        b1 = BaseModel()
+        self.assertIsInstance(b1.created_at, datetime)
+
+    def test_ContainsUpdated_at(self):
+        """Checks `updated_at` attribute existence"""
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "updated_at"))
+
+    def test_Updated_atInstance(self):
+        """Check `updated_at` attribute type"""
+        b1 = BaseModel()
+        self.assertIsInstance(b1.updated_at, datetime)
+
+
+class TestBaseModel_Instance_Print(unittest.TestCase):
+    """Unittest for testing the return value of __str__ method."""
+
+    def test_str_return(self):
+        b1 = BaseModel()
+        ret = "[{}] ({}) {}".format("BaseModel", b1.id, str(b1.__dict__))
+        self.assertEqual(str(b1), ret)
+
+
+class TestBaseModel_Save_Method(unittest.TestCase):
+    """Unittest for testing the save method."""
+
+    def test_validates_save(self):
+        """Check save models"""
+        b1 = BaseModel()
+        updated_at_1 = b1.updated_at
+        b1.save()
+        updated_at_2 = b1.updated_at
+        self.assertNotEqual(updated_at_1, updated_at_2)
+
+
+class TestBaseModel_to_Dict_Method(unittest.TestCase):
+    """Unittest for testing the to_dict method."""
+
+    def test_className_present(self):
+        """Test className present"""
+        b1 = BaseModel()
+        dic = b1.to_dict()
+        self.assertNotEqual(dic, b1.__dict__)
+
+    def test_attribute_ISO_format(self):
+        """Test datetime field isoformated"""
+        b1 = BaseModel()
+        dic = b1.to_dict()
+        self.assertEqual(type(dic['created_at']), str)
+        self.assertEqual(type(dic['updated_at']), str)
+
+
+class TestBaseModel_Kwargs(unittest.TestCase):
+    """Unittest for instantiating BaseModel from kwargs"""
+
+    def test_recreate(self):
+        """Test BaseModel instantiation from kwargs"""
+        b1 = BaseModel()
+        b1.my_number = 89
+
+        b2 = BaseModel(**b1.to_dict())
+        self.assertEqual(b1.id, b2.id)
+        self.assertEqual(b1.my_number, b2.my_number)
+        self.assertEqual(b2.created_at.isoformat(), b2.created_at.isoformat())
+        self.assertEqual(b1.updated_at.isoformat(), b2.updated_at.isoformat())
+
+
+if __name__ == "__main__":
+    unittest.main()
